99c209cccdbffcc9713d278803093c8c1be8f091
flechaVivas <facundovivas15@gmail.com>
2024-03-19 10:57:38 -0300

    Add permission class for app-users

--- adminsight_api/permissions.py
+++ adminsight_api/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework import permissions
+from .models import System, AppUserSystem
 
 
 class IsAdminUser(permissions.BasePermission):
@@ -19,11 +20,17 @@ class IsAdminOrReadOnly(permissions.BasePermission):
         return request.user and request.user.is_staff
 
 
-class IsAuthenticatedOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
         # Permitir acciones de lectura (GET) a todos los usuarios autenticados
         if request.method in permissions.SAFE_METHODS:
-            return request.user and request.user.is_authenticated
+            return True
 
-        # Permitir acciones de escritura (POST, PUT, PATCH, DELETE) solo a administradores
-        return request.user and request.user.is_staff
+        # Permitir acciones de escritura (POST, PUT, PATCH, DELETE) en objetos System y AppUserSystem
+        if isinstance(obj, System):
+            return obj.app_users.filter(id=request.user.id).exists()
+        elif isinstance(obj, AppUserSystem):
+            return obj.app_user == request.user
+
+        # No permitir otras acciones de escritura
+        return False
