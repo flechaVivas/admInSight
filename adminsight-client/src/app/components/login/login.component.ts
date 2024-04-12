8bc2e0a5c94beb3b332463d8438bc8435b3d791b
flechaVivas <facundovivas15@gmail.com>
2024-04-12 15:47:58 -0300

    register, login server error handling

--- adminsight-client/src/app/components/login/login.component.ts
+++ adminsight-client/src/app/components/login/login.component.ts
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   emailError: string = '';
   passwordError: string = '';
   loginError: string = '';
-
+  isLoading: boolean = false; // Nueva variable para controlar el spinner
 
   constructor(
     private authService: AuthService,
@@ -31,14 +31,17 @@ export class LoginComponent implements OnInit {
     this.emailError = '';
     this.passwordError = '';
     this.loginError = '';
+    this.isLoading = true; // Mostrar el spinner
 
     if (!this.email) {
       this.emailError = 'Por favor, complete el campo de email.';
+      this.isLoading = false; // Ocultar el spinner
       return;
     }
 
     if (!this.password) {
       this.passwordError = 'Por favor, complete el campo de contraseña.';
+      this.isLoading = false; // Ocultar el spinner
       return;
     }
 
@@ -49,15 +52,15 @@ export class LoginComponent implements OnInit {
 
     this.authService.login(user.email, user.password).subscribe({
       next: (data) => {
+        this.isLoading = false; // Ocultar el spinner
         // Redirige al usuario a la página de inicio o a otra ruta después del inicio de sesión exitoso
         this.router.navigate(['/login-server']);
       },
       error: (error) => {
+        this.isLoading = false; // Ocultar el spinner
         // Maneja el error de inicio de sesión
         this.loginError = 'Usuario y/o contraseña no son correctos.';
       }
     });
   }
-
-
 }
--- adminsight-client/src/app/components/register-server/register-server.component.ts
+++ adminsight-client/src/app/components/register-server/register-server.component.ts
@@ -9,6 +9,9 @@ import { SystemService } from '../../services/systems.service';
 })
 export class RegisterServerComponent {
   registerForm: FormGroup;
+  isLoading: boolean = false;
+  isSuccess: boolean = false;
+  credentialsError: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,17 +28,32 @@ export class RegisterServerComponent {
 
   onSubmit() {
     if (this.registerForm.valid) {
+      this.isLoading = true;
+      this.isSuccess = false;
+      this.credentialsError = ''; // Limpiar el mensaje de error
+
       const formData = this.registerForm.value;
       this.systemService.registerServer(formData)
         .subscribe(
           response => {
             console.log('Server registered successfully:', response);
+            setTimeout(() => {
+              this.isLoading = false;
+              this.isSuccess = true;
+              setTimeout(() => {
+                window.location.reload();
+              }, 1000);
+            }, 1000);
           },
           error => {
             console.error('Error registering server:', error);
+            this.isLoading = false;
+            this.isSuccess = false;
+            if (error.status === 400) {
+              this.credentialsError = 'Invalid SSH credentials';
+            }
           }
         );
     }
   }
-
 }
