b8737754cd51d32cfb819d8ef93419b0842b2068
flechaVivas <facundovivas15@gmail.com>
2024-05-04 19:35:47 -0300

    loading spinner implemented

--- adminsight-client/src/app/components/modals/password-modal/password-modal.component.ts
+++ adminsight-client/src/app/components/modals/password-modal/password-modal.component.ts
@@ -11,14 +11,20 @@ export class PasswordModalComponent {
   @Output() cancel = new EventEmitter();
 
   sudoPassword: string = '';
+  isLoading: boolean = false;
 
   onConfirm() {
-    this.confirm.emit(this.sudoPassword);
-    this.sudoPassword = '';
+    this.isLoading = true;
+    setTimeout(() => {
+      this.confirm.emit(this.sudoPassword);
+      this.sudoPassword = '';
+      this.isLoading = false;
+    }, 1000);
   }
 
   onCancel() {
     this.cancel.emit();
     this.sudoPassword = '';
+    this.isLoading = false;
   }
 }
